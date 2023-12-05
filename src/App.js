a06dccb15676eabab738a179af8c622aaad63c2d
Jackwtp <marialuiza7655@gmail.com>
2023-12-05 10:55:22 -0300

    exercicio 2

--- src/App.js
+++ src/App.js
@@ -5,23 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>1- instalar o Node.js</p>
-        <p>2- criar uma pasta</p>
-        <p>3- abrir o terminal e executar npm create-react-app minha_pasta</p>
-        <p>4- navegar para dentro da pasta pelo terminal e digitar npm start</p>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Luiza</p>
       </header>
     </div>
   );
